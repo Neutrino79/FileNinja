4ab9d4c5837ef63b5d76ec371cf658bc907a16ac
prathameshkhade <pkhade2865@gmail.com>
2024-03-30 15:32:31 +0000

    fixed - installation stuck on building wheels while installing PyMuPDF
    modified - install.sh -> install swing; \n updatated - requirements.txt

--- install.sh
+++ install.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 
+# install swig
+echo "[+] Installing swing...\n\n"
+sleep 2
+sudo apt install -y swig
+
+if [[ $? -eq 0 ]];
+then
+	sleep 1
+	echo "\n\n[+] Swig installed successfully...\n"
+	sleep 1 && clear
+else
+	echo "\n\n[!] Error in instaling swig! Please retry!"
+	exit 1
+fi
+
+
 # install dependacies
-printf "[+] Installing dependacies...\n"
-sleep 1
+printf "[+] Installing dependacies...\n\n"
+sleep 2
 pip3 install -r requirements.txt
 
 if [[$? == 0]];
 then
 	sleep 1
 	clear
-	printf "\n[✓] Installed sucessfully!"
+	printf "\n\n[✓] Installed sucessfully!"
 else
-	printf "\n[!] Something went wrong!"
+	printf "\n\n[!] Something went wrong!"
 	printf "\n[!] Please try again!"
 fi
